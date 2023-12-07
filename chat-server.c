589a4b4ab0e4ec077d9220d428876d3a41cd3f2d
Liem <jliem@weathertop.cs.middlebury.edu>
2023-12-07 12:44:28 -0500

    struct

--- chat-server.c
+++ chat-server.c
@@ -1,1 +1,21 @@
-printf("Hello world");
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+typdef struct client {
+	char* name;
+	_	ip;
+	char* port;
+	int connfd;
+	void* next;
+} client;
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
