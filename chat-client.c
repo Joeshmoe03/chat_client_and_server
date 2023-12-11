239bccaf799cfacc86250b3277a7cb8deabcef05
Solano <hsolano@basin.cs.middlebury.edu>
2023-12-11 02:48:45 -0500

    added client

--- chat-client.c
+++ chat-client.c
@@ -1,6 +1,4 @@
-/*
- * chat-client.c
- */
+/* chat-client.c */
 
 #include <stdlib.h>
 #include <sys/types.h>
@@ -10,52 +8,122 @@
 #include <stdio.h>
 #include <netdb.h>
 #include <string.h>
+#include <pthread.h>
+#include <time.h>
 
 #define BUF_SIZE 4096
 
+static int connfd;
+
+/* Functions so a single client can concurrenly send/recv data */
+void *sendfunc(void *data) {
+
+	/* Buffer for message and its respective length */
+	char msg[BUF_SIZE+1];
+	int n; 
+
+	/* Infinite loop for send() */
+	while((n = read(0, msg, BUF_SIZE)) > 0) {
+		if (n < 0) {
+			perror("read");
+			continue; //break; exit; are alternatives
+		}
+
+		msg[n] = '\0'; //SUS?
+		send(connfd, msg, n, 0);
+	}
+
+	//SUS: to send server EOF
+	msg[0] = '\0';
+	send(connfd, msg, 0, 0);
+	printf("Exiting.");
+
+	return NULL;
+}
+
+void *recvfunc(void *data) {
+
+	/* Buffer to store incoming message info */
+	char msg[BUF_SIZE+1];
+	// char output[BUF_SIZE];
+	int bytes_received;
+
+	/* Infinite loop for recv() */
+	while(1) {
+
+		/*upon server termination using, clients will terminate */
+		if((bytes_received = recv(connfd, msg, BUF_SIZE, 0)) == 0) {
+			puts("Connection closed by remote host.");
+			exit(EXIT_SUCCESS); //pthread exit instead?
+		}
+
+		puts(msg);
+
+		/* format the things to print: message preceeded by timestamp; modify msg in server? - TODO: ACTUALLY DO ALL OF THIS IN THE SERVER
+			since it has to go to all clients */
+		// msg[bytes_received] = '\0'; /* SUPER IMPORTANT*/
+		time_t timeval = time(NULL);
+		struct tm *tmstruct = localtime(&timeval);        
+		snprintf(output, 4135, "%d:%d:%d: %s", tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec , msg);
+		// puts(output);
+	}
+
+	return NULL;  
+}
+
+
 int main(int argc, char *argv[])
 {
-    char *dest_hostname, *dest_port;
-    struct addrinfo hints, *res;
-    int conn_fd;
-    char buf[BUF_SIZE];
-    int n;
-    int rc;
+	char *dest_hostname, *dest_port;
+	struct addrinfo hints, *res;
+	int rc;
 
-    dest_hostname = argv[1];
-    dest_port     = argv[2];
+	void *retval;
 
-    /* create a socket */
-    conn_fd = socket(PF_INET, SOCK_STREAM, 0);
+	dest_hostname = argv[1];
+	dest_port     = argv[2];
 
-    /* client usually doesn't bind, which lets kernel pick a port number */
+	/* create a socket */
+	connfd = socket(PF_INET, SOCK_STREAM, 0);
+	if(connfd < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    /* but we do need to find the IP address of the server */
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    if((rc = getaddrinfo(dest_hostname, dest_port, &hints, &res)) != 0) {
-        printf("getaddrinfo failed: %s\n", gai_strerror(rc));
-        exit(1);
-    }
+	/* client usually doesn't bind, which lets kernel pick a port number */
 
-    /* connect to the server */
-    if(connect(conn_fd, res->ai_addr, res->ai_addrlen) < 0) {
-        perror("connect");
-        exit(2);
-    }
+	/* but we do need to find the IP address of the server */
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	if((rc = getaddrinfo(dest_hostname, dest_port, &hints, &res)) != 0) {
+		printf("getaddrinfo failed: %s\n", gai_strerror(rc));
+		exit(EXIT_FAILURE);
+	}
 
-    printf("Connected\n");
+	/* connect to the server */
+	if(connect(connfd, res->ai_addr, res->ai_addrlen) < 0) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
 
-    /* infinite loop of reading from terminal, sending the data, and printing
-     * what we get back - THE SAUCE */
-    while((n = read(0, buf, BUF_SIZE)) > 0) {
-        send(conn_fd, buf, n, 0);
+	printf("Connected\n");
 
-        n = recv(conn_fd, buf, BUF_SIZE, 0);
-        printf("received: ");
-        puts(buf);
-    }
+	/* create separate threads for send & recv */
+	pthread_t sendthread, recvthread;
+	if(pthread_create(&sendthread, NULL, sendfunc, NULL) != 0) {
+		perror("pthread_create");
+	}
+	if(pthread_create(&recvthread, NULL, recvfunc, NULL) !=0) {
+		perror("pthread_create");
+	}
 
-    close(conn_fd);
+	/*join threads, so they are ensured to finish their duty ?*/
+	if(pthread_join(sendthread, &retval) != 0) {
+		perror("pthread_join");
+	}
+	//pthread_join(recvthread, &retval); // won't work b/cuz the loop in recvthread is infinite so that thread will never finish, think it's fine
+	//make it detached? or make it exit? or do we even need this?
+
+	close(connfd);
 }
